6dcb2df7dffab89b2b400f82f723c7b548929d22
yassinOrlando <yassinovp@gmail.com>
2021-01-15 14:02:56 -0600

    Logo in svg added to the navBar and images replaced for images of Don Ramon tequila from internet

--- js/init.js
+++ js/init.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
       <div class="col s12 m4 l3 center">
         <div class="card">
             <div class="card-image">
-                <img src="${prod.foto}" width="100%" height="300" alt="producto">
+                <img src="${prod.foto}" width="100%" alt="producto">
                 <a class="btn-floating halfway-fab waves-effect waves-light lime darken-4 modal-trigger"
                     href="#modal1" onclick="getProdInfo('${prod.nombre}')"><i class="material-icons">remove_red_eye</i></a>
             </div>
@@ -39,12 +39,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 //Function to get the selected info if the enterprise
-function getEnterpriseInfo(selectedInfo){
+function getEnterpriseInfo(selectedInfo) {
   let enterpriseInfo
   let modal = document.querySelector('#modal1')
 
-  enterpriseInfoList.forEach((element)=>{
-    if(element.titulo == selectedInfo){
+  enterpriseInfoList.forEach((element) => {
+    if (element.titulo == selectedInfo) {
       enterpriseInfo = element
       return
     }
@@ -79,25 +79,25 @@ function getProdInfo(selectedProd) {
           <h4 class="col s11">${prodInfo.nombre}</h4>
           <a href="#!" class="col s1 modal-close waves-effect waves-red btn-flat exit">X</a>
       </div>
-      <p>Capacidad: ${prodInfo.capacidad}</p>
+      <p><b>Capacidad:</b> ${prodInfo.capacidad}</p>
       <h5 class="green-text">${prodInfo.precio}</h5>
       <p>${prodInfo.info}</p>
       <p>
         <b> Específicos </b><br>
-        Clase: ${prodInfo.especificos.clase}<br>
-        Graduación: ${prodInfo.especificos.graduacion}<br>
-        Barrica: ${prodInfo.especificos.barrica}<br>
-        Reposo: ${prodInfo.especificos.tmpReposo}
+        <b>Clase:</b> ${prodInfo.especificos.clase}<br>
+        <b>Graduación:</b> ${prodInfo.especificos.graduacion}<br>
+        <b>Barrica:</b> ${prodInfo.especificos.barrica}<br>
+        <b>Reposo:</b> ${prodInfo.especificos.tmpReposo}
       </p>
       <p>
         <b> Ficha organoléptica </b><br>
-        Color: ${prodInfo.organoleptica.color}<br>
-        Aroma: ${prodInfo.organoleptica.aroma}<br>
-        Barrica: ${prodInfo.organoleptica.gusto}<br>
+        <b>Color:</b> ${prodInfo.organoleptica.color}<br>
+        <b>Aroma:</b> ${prodInfo.organoleptica.aroma}<br>
+        <b>Barrica:</b> ${prodInfo.organoleptica.gusto}<br>
       </p>
-      <div class="modal-footer">
-        <p>Botella hecha por artesanos</p>
-      </div>
+    </div>
+    <div class="modal-footer">
+      <a class="btn-flat">Botella hecha por artesanos</a>
     </div>
   `
 }
@@ -122,11 +122,11 @@ const enterpriseInfoList = [
 // Product list with details
 const prodList = [
   {
-    foto: "https://picsum.photos/200/400",
+    foto: "https://www.chedraui.com.mx/medias/638478000064-00-CH1200Wx1200H?context=bWFzdGVyfHJvb3R8ODE4ODd8aW1hZ2UvanBlZ3xoZWIvaDdiLzEwMjQwNDk2ODYxMjE0LmpwZ3xlMTM1NGUxMGVjOGZjMmZhYmI3ODAxZGUyMzA1NzAwZTZiYTc0NTRiZTM5OWQ2NmRjMjY0ZWNiMDJkNTExY2Qy",
     nombre: "Tequila reposado 50ml",
     capacidad: "0.5 ltr",
-    precio: "79 MXN",
-    info: "100% Agave. Reposado",
+    precio: "79.75 MXN",
+    info: "100% Agave. Reposado (Pedido mínimo de una caja con 48 piezas)",
     especificos: {
       clase: "Extra añejo",
       graduacion: "40% Alc. Vol",
@@ -140,11 +140,11 @@ const prodList = [
     }
   },
   {
-    foto: "https://picsum.photos/200/400",
+    foto: "https://www.chedraui.com.mx/medias/638478000163-00-CH1200Wx1200H?context=bWFzdGVyfHJvb3R8OTk4Njh8aW1hZ2UvanBlZ3xoMTAvaDljLzEwMjQwNTE1MjQ0MDYyLmpwZ3wxNzg2MDdmZjY0NDE1NmY1ZjU2MzVkMjBkNTc5ODc5MGZiYjIyNWE5N2I3NDAxM2JlYzE5NzA1YWMwOTcyYzhj",
     nombre: "Tequila reposado 100ml",
     capacidad: "0.5 ltr",
     precio: "79 MXN",
-    info: "100% Agave. Reposado",
+    info: "100% Agave. Reposado (Pedido mínimo de una caja con 48 piezas)",
     especificos: {
       clase: "Extra añejo",
       graduacion: "40% Alc. Vol",
